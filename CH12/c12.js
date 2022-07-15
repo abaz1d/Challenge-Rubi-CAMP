4dae6b301d90db3ae55f4b5a84519ffeb7887fe8
Abdul Aziz <abaz.id@MacBook-Pro.local>
2022-07-15 20:49:42 +0700

    Challenge  12

--- CH12/c12.js
+++ CH12/c12.js
@@ -1,11 +1,8 @@
 // dataBase = [{"definition": "siapa presiden indonesia?", "term": "jokowi"}, {"definition": "kapan indonesia merdeka?", "term": "1945"}]
 // greeting sekaligus memanggil fs dan readline
 if (process.argv[2] === undefined) {
-    console.log('Tolong sertakan nama file sebagai inputan soalnya contohnya : \'node solution.js data.json\'.');
-
-
-}
-else {
+    console.log('Tolong sertakan nama file sebagai inputan soalnya contohnya : \'node solution.js data.json\'.')
+} else {
 
     console.log('Nah bener silahkan diisi');
 
