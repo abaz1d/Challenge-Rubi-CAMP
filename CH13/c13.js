f36e64777c23bd86b61b079fb567be30b63409e8
Abdul Aziz <abaz.id@MacBook-Pro.local>
2022-07-16 16:56:08 +0700

    Challenge13

---
+++ CH13/c13.js
@@ -0,0 +1,16 @@
+if (process.argv[2] === undefined) {
+    console.log(">>>JS TODO<<<" +
+    "\n node todo.js <comand>" +
+    "\n node todo.js list" +
+    "\n node todo.js task <task_id>" +
+    "\n node todo.js add <task content>" +
+    "\n node todo.js delete <task_id>" +
+    "\n node todo.js complete <task_id>" +
+    "\n node todo.js uncomplete <task_id>" +
+    "\n node todo.js list:outstanding asc|desc" +
+    "\n node todo.js list:complete asc|desc" +
+    "\n node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>" +
+    "\n node todo.js filter:<tag_name>")
+} else {
+    
+}
