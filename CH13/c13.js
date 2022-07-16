7a3d7e9695c4ce75799bf2e51982fd7f5e8ac728
Abdul Aziz <abaz.id@MacBook-Pro.local>
2022-07-16 17:33:31 +0700

    Challenge13

--- CH13/c13.js
+++ CH13/c13.js
@@ -1,16 +1,28 @@
-if (process.argv[2] === undefined) {
-    console.log(">>>JS TODO<<<" +
-    "\n node todo.js <comand>" +
-    "\n node todo.js list" +
-    "\n node todo.js task <task_id>" +
-    "\n node todo.js add <task content>" +
-    "\n node todo.js delete <task_id>" +
-    "\n node todo.js complete <task_id>" +
-    "\n node todo.js uncomplete <task_id>" +
-    "\n node todo.js list:outstanding asc|desc" +
-    "\n node todo.js list:complete asc|desc" +
-    "\n node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>" +
-    "\n node todo.js filter:<tag_name>")
-} else {
-    
+const fs = require('fs')
+const input = process.argv;
+const data = fs.readFileSync(data.json, 'utf-8')
+
+switch (process.argv[2]) {
+    case undefined:
+        console.log(
+            ">>>JS TODO<<<" +
+            "\n node todo.js <comand>" +
+            "\n node todo.js list" +
+            "\n node todo.js task <task_id>" +
+            "\n node todo.js add <task content>" +
+            "\n node todo.js delete <task_id>" +
+            "\n node todo.js complete <task_id>" +
+            "\n node todo.js uncomplete <task_id>" +
+            "\n node todo.js list:outstanding asc|desc" +
+            "\n node todo.js list:complete asc|desc" +
+            "\n node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>" +
+            "\n node todo.js filter:<tag_name>"
+            );
+        break;
+    case 'add':
+        let output = '';
+for(let i = 3, i < input.length, i++){
+    output += input[i] + ''
 }
+}
+
