3a5f5f9a061534bc2c690b9297112da13f3cbc46
Abdul Aziz <abaz.id@MacBook-Pro.local>
2022-08-21 07:31:30 +0700

    Challenge 22

--- CH22/routes/index.js
+++ CH22/routes/index.js
@@ -41,7 +41,7 @@ module.exports = function (db) {
         }
 
         if (req.query.boolean && req.query.booleanCheck == 'on') {
-            wheres['boolean'] = JSON.parse(req.query.boolean)
+            wheres['boolean'] = req.query.boolean
         }
 
 
@@ -70,7 +70,7 @@ module.exports = function (db) {
             integer: parseInt(req.body.integer),
             float: JSON.parse(req.body.float),
             date: new Date(`${req.body.date}`),
-            boolean: JSON.parse(req.body.boolean)
+            boolean: req.body.boolean
         };
         db.collection("users").insertOne(myobj, function (err, res) {
             if (err) throw err;
@@ -105,7 +105,7 @@ module.exports = function (db) {
             integer: parseInt(req.body.integer),
             float: JSON.parse(req.body.float),
             date: new Date(`${req.body.date}`),
-            boolean: JSON.parse(req.body.boolean)
+            boolean: req.body.boolean
         };
         db.collection("users").updateOne({ "_id": ObjectId(`${req.params.id}`) }, { $set: myobj }, function (err) {
             if (err) throw err;
@@ -114,60 +114,6 @@ module.exports = function (db) {
 
     })
 
-    router.get('/api', async function (req, res,) {
-        const url = req.url == '/' ? '/?page=1' : req.url;
-        const page = req.query.page || 1;
-        const limit = 3;
-        const offset = (page - 1) * limit;
-        const wheres = {}
-        const filter = `&idCheck=${req.query.idCheck}&id=${req.query.id}&stringCheck=${req.query.stringCheck}&string=${req.query.string}&integerCheck=${req.query.integerCheck}&integer=${req.query.integer}&floatCheck=${req.query.floatCheck}&float=${req.query.float}&dateCheck=${req.query.dateCheck}&startDate=${req.query.startDate}&endDate=${req.query.endDate}&booleanCheck=${req.query.booleanCheck}&boolean=${req.query.boolean}`
-        var sortBy = req.query.sortBy == undefined ? 'string' : req.query.sortBy;
-        var sortMode = req.query.sortMode == undefined ? 1 : req.query.sortMode;
-        var sortMongo = JSON.parse(`{"${sortBy}" : ${sortMode}}`);
-
-        if (req.query.string && req.query.stringCheck == 'on') {
-            wheres["string"] = new RegExp(`${req.query.string}`, 'i')
-        }
-
-        if (req.query.integer && req.query.integerCheck == 'on') {
-            wheres['integer'] = parseInt(req.query.integer)
-        }
-
-        if (req.query.float && req.query.floatCheck == 'on') {
-            wheres['float'] = JSON.parse(req.query.float)
-        }
-
-        if (req.query.dateCheck == 'on') {
-            if (req.query.startDate != '' && req.query.endDate != '') {
-                wheres['date'] = { $gte: new Date(`${req.query.startDate}`), $lte: new Date(`${req.query.endDate}`) }
-            }
-            else if (req.query.startDate) {
-                wheres['date'] = { $gte: new Date(`${req.query.startDate}` )}
-            }
-            else if (req.query.endDate) {
-                wheres['date'] = { $lte: new Date(`${req.query.endDate}`) }
-            }
-        }
-
-        if (req.query.boolean && req.query.booleanCheck == 'on') {
-            wheres['boolean'] = JSON.parse(req.query.boolean)
-        }
-
-
-        db.collection("users").find(wheres).toArray(function (err, result) {
-            if (err) {
-                console.error(err);
-            }
-            var total = result.length;
-            const pages = Math.ceil(total / limit)
-            db.collection("users").find(wheres).skip(offset).limit(limit).collation({ 'locale': 'en' }).sort(sortMongo).toArray((err, data) => {
-                if (err) {
-                    console.error(err)
-                }
-                res.json({ data, pages, page })
-            })
-        })
-    })
     return router;
 }
 
